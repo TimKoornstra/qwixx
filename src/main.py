bb9e6b3b5712a8b47c405e07bf10ed1867adf39e
Tim <tim.koornstra@gmail.com>
2022-04-25 12:09:14 +0200

    reset main.py

--- src/main.py
+++ src/main.py
@@ -3,9 +3,5 @@
 from Qwixx import *
 
 if __name__ == "__main__":
-    Q = Qwixx(1, "Tim")
-    Q.players[0].mark_row("Red", 8)
-    Q.players[0].mark_row("Yellow", 8)
-    Q.players[0].mark_row("Green", 8)
-    Q.players[0].mark_row("Blue", 8)
+    Q = Qwixx(4)
     Q.play()
